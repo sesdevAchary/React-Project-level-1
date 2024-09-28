56fcaa35cd9d3401a0b0e3ebf235e90269118681
sesdevAchary <sesdev.achary@gmail.com>
2024-09-28 10:31:15 +0000

    o.25 project

---
+++ axios-app/src/App.js
@@ -0,0 +1,11 @@
+import PersonList from './component/personList.js';
+
+function App() {
+  return (
+    <div className="App">
+      <PersonList />
+    </div>
+  );
+}
+
+export default App;
---
+++ axios-app/src/component/personList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const personList = () => {
+
+    return (
+        <div className="person-list">
+          <h2>Person List</h2>
+          </div>
+    )
+}
+export default personList;
