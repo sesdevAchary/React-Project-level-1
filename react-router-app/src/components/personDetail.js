bb66e6b8a1c6446c24bc2a8d0a67845a9a1b4b53
sesdevAchary <sesdev.achary@gmail.com>
2024-10-03 05:18:25 +0000

    reac-router-app

---
+++ react-router-app/src/components/personDetail.js
@@ -0,0 +1,11 @@
+import react from 'react'
+
+const personDetail=()=>{
+    return (
+        <div>
+            <h2>
+                TO add details about perso </h2></div>
+    )
+}
+
+export default personDetail;
