b352883e7a5f7df9ce3386697e3cdbd225ace8c2
sesdevAchary <sesdev.achary@gmail.com>
2024-10-04 08:21:28 +0000

    react router app

--- react-router-app/src/components/personList.js
+++ react-router-app/src/components/personList.js
@@ -1,6 +1,8 @@
 import React from 'react'
 //import axios from 'axios'
 
+import '../styles/personList.css';
+
 const API_URL = process.env.REACT_APP_API_URL
 
 const personList = () => {
@@ -8,7 +10,7 @@ const personList = () => {
 
   console.log(API_URL)
   return (
-    <div>
+    <div class="person-list">
       <h2>
         To edit  a person list</h2></div>
   )
