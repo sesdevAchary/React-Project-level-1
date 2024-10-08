8b81676ee9f907b0da3c2bd99b3357a2e072cf2a
sesdevAchary <sesdev.achary@gmail.com>
2024-10-08 18:09:51 +0000

    react-router-lab

--- react-router-app/src/components/notFound.js
+++ react-router-app/src/components/notFound.js
@@ -3,7 +3,7 @@ import React from 'react';
 const NotFound = () => {
   return (
     <div className="not-found">
-      <h2>404 - Not Found</h2>
+      <h2>404 - Not Found the content you want to search</h2>
       <p>The page you are looking for does not exist.</p>
       <a href="/" className="btn">Go Back Home</a>
     </div>
