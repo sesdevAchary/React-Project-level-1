010f70f062389cb9f51e846159588c8f890de9ac
sesdevAchary <sesdev.achary@gmail.com>
2024-10-17 17:16:47 +0000

    movie trailer showing feature

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,3 +1,4 @@
 import './App.css' ; // importing the css app//
 import {usestate} from 'react'// to manage states in functional component
 import ReactPlayer from 'react-player'; // component for playing video around different source
+import movieTrailer from 'movie-trailer'; // fetches the URL of the given movie trailer 
