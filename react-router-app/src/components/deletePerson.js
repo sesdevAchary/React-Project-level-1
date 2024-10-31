02e668593a56f3a0f44502d357cac17c938dab61
sesdevAchary <sesdev.achary@gmail.com>
2024-10-31 11:18:40 +0000

    react-router-app

--- react-router-app/src/components/deletePerson.js
+++ react-router-app/src/components/deletePerson.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const deletePerson = ({ id, onDelete }) => {
+const deletePerson = ({ id, onDelete }) => { {/* functional component with 2 props*/}
   const handleDelete = () => {
     if (window.confirm('Are you sure you want to delete this person?')) {
       onDelete(id);
@@ -15,3 +15,9 @@ const deletePerson = ({ id, onDelete }) => {
 };
 
 export default deletePerson;
+
+
+
+/* The DeletePerson component provides a button for deleting a person. When clicked,
+ it prompts the user for confirmation and, if confirmed, 
+ invokes a provided onDelete function with the person's id.*/
