b100b4f646588ab9406906f09b293f2ba8fa14ed
sesdevAchary <sesdev.achary@gmail.com>
2024-11-01 17:13:55 +0000

    react-router-app

--- react-router-app/src/components/personDetail.js
+++ react-router-app/src/components/personDetail.js
@@ -10,7 +10,7 @@ const API_URL = process.env.REACT_APP_API_URL
 
 
 const personDetail=()=>{
-  const{id}= useParams(); 
+  const{id}= useParams(); /* to extract id parameter */
 }
 
 export default personDetail;
