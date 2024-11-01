9a2688c8f33402d57408c98778381b6cbec7c579
sesdevAchary <sesdev.achary@gmail.com>
2024-11-01 17:11:29 +0000

    react-router-app

--- react-router-app/src/components/personDetail.js
+++ react-router-app/src/components/personDetail.js
@@ -1,21 +1,16 @@
 import React ,{useEffect,usestate} from 'react'
-import {Link} from 'react-router-dom';
+import {userParams,Link,useNavigate } from 'react-router-dom';
 import axios from 'axios'; 
-
 import Notification from './notification.js'
+
+
 import '../styles/personList.css'
 
 const API_URL = process.env.REACT_APP_API_URL
 
 
 const personDetail=()=>{
-    console.log(API_URL)
-
-    return (
-        <div>
-            <h2>
-                TO add details about person </h2></div>
-    )
+  const{id}= useParams();
 }
 
 export default personDetail;
