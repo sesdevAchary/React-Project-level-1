59a2222a6b11bf31e19465ea8d5de1605522bedd
sesdevAchary <sesdev.achary@gmail.com>
2025-05-29 17:24:52 +0000

    saving the changes

--- aNewPythonEra/first.py
+++ aNewPythonEra/first.py
@@ -2,3 +2,12 @@ a= 5
 b=3
 sum = a+b
 print("sum =", sum)
+
+
+a= int(input("enter your number= "))
+b= int(input(" enter your second number="))
+
+if a>b
+ print("larger num is a:",a)
+ else 
+ print("larger number is b:",b)
