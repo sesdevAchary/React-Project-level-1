d8d7e017b56cb33676c48b0b5d8cc85638c4dfd3
sesdevAchary <sesdev.achary@gmail.com>
2025-05-29 17:37:02 +0000

    code run ok

--- aNewPythonEra/first.py
+++ aNewPythonEra/first.py
@@ -1,13 +1,29 @@
-a= 5
-b=3
-sum = a+b
-print("sum =", sum)
+# a= 5
+# b=3
+# sum = a+b
+# print("sum =", sum)
 
 
-a= int(input("enter your number= "))
-b= int(input(" enter your second number="))
+# a= int(input("enter your number= "))
+# b= int(input(" enter your second number="))
 
-if a > b:
-  print("larger num is a:",a)
+# if a > b:
+#   print("larger num is a:",a)
+# else:
+#   print("larger number is b:",b)
+
+
+
+
+a= int(input(" first number is "))
+b= int(input("second numeber is "))
+c= int(input("third number is"))
+
+if a>b:
+  priint(a)
+if b>c:
+    print(b)
+if a>c:
+    print(c)
 else:
-  print("larger number is b:",b)
+    print("NOTHING")
