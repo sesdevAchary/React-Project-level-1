75c106ff9d3fae75f6ff420971bf6201b931cc3a
sesdevAchary <sesdev.achary@gmail.com>
2025-06-03 10:19:51 +0000

    second star pattern printed

--- aNewPythonEra/first.py
+++ aNewPythonEra/first.py
@@ -154,13 +154,19 @@
 
 
 
-n= int(input("enter the number you want: "))
-for i in range (1,n+1):
-         if(i==1 or i==n):
-                 print("*"*n)
-         else:
-                 print("*", end="")
-                 print(" "*(n-2), end="")
-                 print("*")
+# n= int(input("enter the number you want: "))
+# for i in range (1,n+1):
+#          if(i==1 or i==n):
+#                  print("*"*n)
+#          else:
+#                  print("*", end="")
+#                  print(" "*(n-2), end="")
+#                  print("*")
         
   
+
+
+n= int(input("enter the number you want: "))
+for i in range (1,n+1):
+        print(" "* (n-i),end=" ")
+        print("*"*(2*i-1))
