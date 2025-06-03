a7a1222926152f839e670587c8a0e33e3cd201d3
sesdevAchary <sesdev.achary@gmail.com>
2025-06-03 07:05:27 +0000

    errror resolved

--- aNewPythonEra/first.py
+++ aNewPythonEra/first.py
@@ -145,9 +145,9 @@
    
 n = int ( input("enter a number-> "))
 
-for i in range (n):
+for i in range (2,n):
         if(n%i==0):
                 print(" n is not a prime number")
         else:
-         print("n is a prime number")
+                 print("n is a prime number")
  
