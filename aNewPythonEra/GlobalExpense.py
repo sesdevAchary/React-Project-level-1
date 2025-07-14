6b16615bb252cdfb8e280e20cb7f9b827e88aafe
sesdevAchary <sesdev.achary@gmail.com>
2025-07-14 17:41:34 +0000

    compressing the list

--- aNewPythonEra/GlobalExpense.py
+++ aNewPythonEra/GlobalExpense.py
@@ -45,3 +45,7 @@ from collections import Counter
 text = "apple orange banana apple orange apple"
 words = text.split()
 print(Counter(words))
+squares = [x**2 for x in range(10)]
+print(squares)
+squares = [x**2 for x in range(10)]
+print(squares)
