04f5c85aeb13d6bc181c83d744a862885ea771cc
sesdevAchary <sesdev.achary@gmail.com>
2025-07-14 17:39:59 +0000

    checkin for prime num

--- aNewPythonEra/GlobalExpense.py
+++ aNewPythonEra/GlobalExpense.py
@@ -29,3 +29,7 @@ def is_palindrome(s):
     return s == s[::-1]
 
 print(is_palindrome("racecar"))  # True
+def is_prime(n):
+    return n > 1 and all(n % i != 0 for i in range(2, int(n**0.5) + 1))
+
+print(is_prime(29))  # True
