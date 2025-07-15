6937f1438f73ee1e315eaa96938764ce5bf31f04
sesdevAchary <sesdev.achary@gmail.com>
2025-07-15 17:53:58 +0000

    finding the duplicate element

--- aNewPythonEra/GlobalExpense.py
+++ aNewPythonEra/GlobalExpense.py
@@ -80,3 +80,7 @@ def countdown(seconds):
     print("Time's up!")
 
 # countdown(5)
+def find_duplicates(lst):
+    return list(set([x for x in lst if lst.count(x) > 1]))
+
+print(find_duplicates([1, 2, 2, 3, 4, 4, 5]))
