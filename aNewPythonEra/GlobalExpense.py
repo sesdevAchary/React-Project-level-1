2aa7765e91afc400876eecd787e5225d4209883a
sesdevAchary <sesdev.achary@gmail.com>
2025-07-16 17:54:31 +0000

    generating the password

--- aNewPythonEra/GlobalExpense.py
+++ aNewPythonEra/GlobalExpense.py
@@ -170,3 +170,10 @@ while True:
         delete_task()
     elif choice == "4":
         break
+import random
+import string
+
+length = int(input("Password length: "))
+chars = string.ascii_letters + string.digits + string.punctuation
+password = ''.join(random.choice(chars) for _ in range(length))
+print("Generated password:", password)
