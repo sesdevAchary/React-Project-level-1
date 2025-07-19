8372247d8a352d77ee08f6214be6a936e1874bb0
sesdevAchary <sesdev.achary@gmail.com>
2025-07-19 17:18:51 +0000

    generating the password form

--- aNewPythonEra/GlobalExpense.py
+++ aNewPythonEra/GlobalExpense.py
@@ -381,3 +381,10 @@ length = int(input("Password length: "))
 characters = string.ascii_letters + string.digits + string.punctuation
 password = ''.join(random.choice(characters) for _ in range(length))
 print("Generated password:", password)
+import random
+import string
+
+length = int(input("Password length: "))
+characters = string.ascii_letters + string.digits + string.punctuation
+password = ''.join(random.choice(characters) for _ in range(length))
+print("Generated password:", password)
