fa7e5ebef5619499d3e6097eb075b9139a0b4c78
sesdevAchary <sesdev.achary@gmail.com>
2025-07-19 17:25:16 +0000

    image classifier

--- aNewPythonEra/GlobalExpense.py
+++ aNewPythonEra/GlobalExpense.py
@@ -492,3 +492,23 @@ while True:
 
 cap.release()
 cv2.destroyAllWindows()
+import tensorflow as tf
+from tensorflow.keras.datasets import cifar10
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten
+
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+model = Sequential([
+    Flatten(input_shape=(32,32,3)),
+    Dense(128, activation='relu'),
+    Dense(10, activation='softmax')
+])
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=5)
+model.evaluate(x_test, y_test)
