ec85c2ecad14ed8aec1fc23ed06d74d99d8659e7
sesdevAchary <sesdev.achary@gmail.com>
2025-07-20 04:48:55 +0000

    added random quotes

--- aNewPythonEra/Personal_assistant.py
+++ aNewPythonEra/Personal_assistant.py
@@ -1,2 +1,12 @@
 ## PERSONAL ASSISTANT CONSOLE APP....
 
+import random   #Used to randomly pick a motivational quote.
+import datetime 
+
+quotes=[
+    "Keep going, you're doing great!",
+    "Stay positive and work hard.",
+    "Success is a journey, not a destination.",
+    "Believe in yourself!",
+    "Every day is a second chance."
+]
