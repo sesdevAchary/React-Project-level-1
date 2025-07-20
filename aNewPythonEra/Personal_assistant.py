cd8d9d2e7bcdcff71e1651972b782a08c13d00f5
sesdevAchary <sesdev.achary@gmail.com>
2025-07-20 04:59:05 +0000

    greeting the user functionality added

--- aNewPythonEra/Personal_assistant.py
+++ aNewPythonEra/Personal_assistant.py
@@ -10,3 +10,9 @@ quotes=[
     "Believe in yourself!",
     "Every day is a second chance."
 ]
+
+
+def greeet_user():
+    name =print(input("👋 Hello! What's your name?"))
+    print(f"Welcome ,{name} ! I am your personal assistant ")
+    return name
