12891649fd2cf4f6803fa62bf86539293942ccbd
sesdevAchary <sesdev.achary@gmail.com>
2025-07-21 18:01:07 +0000

    prime number checker

--- aNewPythonEra/Personal_assistant.py
+++ aNewPythonEra/Personal_assistant.py
@@ -88,6 +88,13 @@ public static boolean isPrime(int n) {
     }
     return true;
 }
+public static boolean isPrime(int n) {
+    if (n <= 1) return false;
+    for (int i = 2; i <= Math.sqrt(n); i++) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
 
 
 
