96bb0f12a6b4c974536c259ccb54c0fc4e9ebc5f
sesdevAchary <sesdev.achary@gmail.com>
2025-07-21 18:13:13 +0000

    fibonacci series generator

--- aNewPythonEra/Personal_assistant.py
+++ aNewPythonEra/Personal_assistant.py
@@ -124,6 +124,15 @@ public static void bubbleSort(int[] arr) {
         }
     }
 }
+public static void printFibonacci(int n) {
+    int a = 0, b = 1;
+    for (int i = 0; i < n; i++) {
+        System.out.print(a + " ");
+        int temp = a + b;
+        a = b;
+        b = temp;
+    }
+}
 
 
 
