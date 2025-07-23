bbce08494198ba2058eac9b3307641467c4ee895
sesdevAchary <sesdev.achary@gmail.com>
2025-07-23 17:24:15 +0000

    setting the main function

--- aNewPythonEra/Personal_assistant.py
+++ aNewPythonEra/Personal_assistant.py
@@ -298,5 +298,12 @@ public static void printFibonacci(int n) {
         b = temp;
     }
 }
+public static boolean isPrime(int n) {
+    if (n <= 1) return false;
+    for (int i = 2; i <= Math.sqrt(n); i++) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
 
 
