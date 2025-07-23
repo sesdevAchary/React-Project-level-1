34eb6653b72a8604b891e9a8827e02662b269990
sesdevAchary <sesdev.achary@gmail.com>
2025-07-23 17:21:03 +0000

    linear search

--- aNewPythonEra/Personal_assistant.py
+++ aNewPythonEra/Personal_assistant.py
@@ -268,5 +268,11 @@ public static boolean isSorted(int[] arr) {
         }
     }
 }
+public static int linearSearch(int[] arr, int target) {
+    for (int i = 0; i < arr.length; i++) {
+        if (arr[i] == target) return i;
+    }
+    return -1;
+}
 
 
