8bc6f4a941c6fc0b5242e6d728d6eac916f08512
sesdevAchary <sesdev.achary@gmail.com>
2025-07-24 17:11:13 +0000

    checkin for sorted array

--- aNewPythonEra/Personal_assistant.py
+++ aNewPythonEra/Personal_assistant.py
@@ -161,3 +161,9 @@ class Person {
         System.out.println("Hi, I'm " + name + ", age " + age);
     }
 }
+public static boolean isSorted(int[] arr) {
+    for (int i = 0; i < arr.length - 1; i++) {
+        if (arr[i] > arr[i + 1]) return false;
+    }
+    return true;
+}
