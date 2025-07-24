c78915adda8bb6abc94bdd2e9a28f91c63bb2ae2
sesdevAchary <sesdev.achary@gmail.com>
2025-07-24 17:06:01 +0000

    findin the gcd

--- aNewPythonEra/Personal_assistant.py
+++ aNewPythonEra/Personal_assistant.py
@@ -127,3 +127,16 @@ while True:
 
         
     
+    
+    
+    
+    
+    
+    public static int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
