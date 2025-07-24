7aac29e03cdabff5c7fc0b810f72ed0b27edac97
sesdevAchary <sesdev.achary@gmail.com>
2025-07-24 17:07:35 +0000

    java cons

--- aNewPythonEra/Personal_assistant.py
+++ aNewPythonEra/Personal_assistant.py
@@ -148,3 +148,16 @@ public static int reverseInt(int num) {
     }
     return rev;
 }
+class Person {
+    String name;
+    int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void greet() {
+        System.out.println("Hi, I'm " + name + ", age " + age);
+    }
+}
