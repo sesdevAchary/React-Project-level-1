ffd0989b81c678d46b0ad1fe8acd88526258dc78
sesdevAchary <sesdev.achary@gmail.com>
2025-07-24 17:06:44 +0000

    reversin the int

--- aNewPythonEra/Personal_assistant.py
+++ aNewPythonEra/Personal_assistant.py
@@ -140,3 +140,11 @@ while True:
     }
     return a;
 }
+public static int reverseInt(int num) {
+    int rev = 0;
+    while (num != 0) {
+        rev = rev * 10 + num % 10;
+        num /= 10;
+    }
+    return rev;
+}
