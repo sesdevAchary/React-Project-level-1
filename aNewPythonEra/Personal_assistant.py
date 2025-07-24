600c3c1ecdf26a90a5cc02ef360ba3cb19df61da
sesdevAchary <sesdev.achary@gmail.com>
2025-07-24 17:16:51 +0000

    fibonaci series

--- aNewPythonEra/Personal_assistant.py
+++ aNewPythonEra/Personal_assistant.py
@@ -184,3 +184,12 @@ a = a - b;
 public static boolean isPalindrome(String str) {
     return str.equals(new StringBuilder(str).reverse().toString());
 }
+public static void printFibonacci(int n) {
+    int a = 0, b = 1;
+    for (int i = 0; i < n; i++) {
+        System.out.print(a + " ");
+        int temp = a + b;
+        a = b;
+        b = temp;
+    }
+}
