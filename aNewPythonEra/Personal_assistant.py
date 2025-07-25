d05c0b40828911e775c6289c7d7ac80bb2f86606
sesdevAchary <sesdev.achary@gmail.com>
2025-07-25 16:41:24 +0000

    fib series spotted

--- aNewPythonEra/Personal_assistant.py
+++ aNewPythonEra/Personal_assistant.py
@@ -140,3 +140,12 @@ public static void calculator(char operator, double a, double b) {
         default: System.out.println("Invalid operator");
     }
 }
+public static void printFibonacci(int n) {
+    int a = 0, b = 1;
+    for (int i = 0; i < n; i++) {
+        System.out.print(a + " ");
+        int temp = a + b;
+        a = b;
+        b = temp;
+    }
+}
