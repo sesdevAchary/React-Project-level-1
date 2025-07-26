704606534d0ada352c8a6493194e75effe7b634a
sesdevAchary <sesdev.achary@gmail.com>
2025-07-26 14:28:00 +0000

    randomly choosin the characters

--- aNewPythonEra/Password.py
+++ aNewPythonEra/Password.py
@@ -6,5 +6,16 @@ import pyperclip
 #Lets the program copy text to the clipboard.to copy the password after generation.
 
 def generate_password(length, use_Letter=True,use_digits=True,use_symbols=True):
-    characters=''
+#include letters (A-Z, a-z),(0-9),!@#$%^&*()
+#If arguments not passed when calling the function, it assumes you want to include that type of character.
+    characters=''#Initializes an empty string to collect valid characters.acc to user
+    if use_Letter:
+        characters=characters+string.ascii_letters
+    #if true then add a-z,A-Z to the empty char
+    if use_digits:
+        characters= characters+string.digits
+    if use_symbols:
+        characters=characters+string.punctuation
+    if not characters:
+        return "❌ Please select at least one character type."
     
