6b928242ca736b4b6ba97dfd95a265e0fc2d96df
sesdevAchary <sesdev.achary@gmail.com>
2025-07-26 17:06:30 +0000

    calculati the user value

--- aNewPythonEra/Password.py
+++ aNewPythonEra/Password.py
@@ -64,3 +64,18 @@ def get_user_pref():
     }
     return a;
 }
+public static void calculator(char operator, double a, double b) {
+    switch (operator) {
+        case '+': System.out.println(a + b); break;
+        case '-': System.out.println(a - b); break;
+        case '*': System.out.println(a * b); break;
+        case '/': System.out.println(b != 0 ? a / b : "Divide by zero!"); break;
+        default: System.out.println("Invalid operator");
+    }
+}
+public static int linearSearch(int[] arr, int target) {
+    for (int i = 0; i < arr.length; i++) {
+        if (arr[i] == target) return i;
+    }
+    return -1;
+}
