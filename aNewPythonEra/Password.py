b3742992a2843cba01368326407e850b3c8f04c0
sesdevAchary <sesdev.achary@gmail.com>
2025-07-27 18:22:32 +0000

    balanced paranthesis checkin

--- aNewPythonEra/Password.py
+++ aNewPythonEra/Password.py
@@ -82,3 +82,15 @@ def fibonacci(n):
     return fibonacci(n-1) + fibonacci(n-2)
 
 print([fibonacci(i) for i in range(10)])
+def is_balanced(s):
+    stack = []
+    mapping = {')': '(', ']': '[', '}': '{'}
+    for char in s:
+        if char in mapping.values():
+            stack.append(char)
+        elif char in mapping:
+            if not stack or mapping[char] != stack.pop():
+                return False
+    return not stack
+
+print(is_balanced("{[()]}"))  # True
