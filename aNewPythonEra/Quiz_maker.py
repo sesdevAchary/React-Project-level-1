b67776d8b79a082a7442dc7ec94c97f4aa964f04
sesdevAchary <sesdev.achary@gmail.com>
2025-07-28 18:09:20 +0000

    basic formal counter func

--- aNewPythonEra/Quiz_maker.py
+++ aNewPythonEra/Quiz_maker.py
@@ -46,3 +46,24 @@ class user:
     
 
 
+
+
+
+
+
+
+
+
+
+
+import { useState } from "react";
+
+function Counter() {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <p>{count}</p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+    </div>
+  );
+}
